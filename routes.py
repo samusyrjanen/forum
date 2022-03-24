c79e0578e5322d6d01be9b1ea2484d2f3e7903a4
Samu Syrjänen <samu.syrjanen@gmail.com>
2022-03-24 12:39:49 +0200

    threads to index.html and links to each individual thread page

--- routes.py
+++ routes.py
@@ -6,3 +6,8 @@ import threads
 def index():
     list = threads.get_thread_list()
     return render_template('index.html', threads=list)
+
+@app.route('/thread/<int:id>')
+def thread(id):
+    thread = threads.get_specific_thread(id)
+    return render_template('thread.html', thread=thread)
--- threads.py
+++ threads.py
@@ -2,7 +2,13 @@ from db import db
 import users
 
 def get_thread_list():
-    sql = 'select T.content, U.username, T.sent_at from threads T, users U where T.user_id=U.id order by T.id'
+    sql = 'select T.content, U.username, T.sent_at, T.id from threads T, ' \
+        'users U where T.user_id=U.id order by T.id'
     result = db.session.execute(sql)
     return result.fetchall()
 
+def get_specific_thread(id):
+    sql = 'select T.content, U.username, T.sent_at from threads T, ' \
+        'users U where T.user_id=U.id and T.id=:id'
+    result = db.session.execute(sql, {'id':id})
+    return result.fetchall()
